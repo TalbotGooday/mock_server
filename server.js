c4b6fe8dcb35ef078c7f1cd560ebbcce87f0dc30
AlexGooday <alexey.mostovoy.w@gmail.com>
2023-10-25 15:20:09 +0300

    Updated login validation

--- server.js
+++ server.js
@@ -9,8 +9,40 @@ server.use(jsonServer.rewriter({
    '/api/*': '/$1',
    '/policies/': '/policy',
    '/policies/email/': '/policies',
-   '/auth-service/oauth/token/': '/login'
+   '/auth-service/oauth/token?username=:email': '/users/:email/login?singular=1'
 }))
+
+server.use(jsonServer.bodyParser)
+
+server.use(function (req, res, next) {
+  const _send = res.send
+  res.send = function (body) {
+    if (require('url').parse(req.originalUrl, true).query['singular']) {
+      try {
+        const json = JSON.parse(body)
+        if (Array.isArray(json)) {
+          if (json.length === 1) {
+            return _send.call(this, JSON.stringify(json[0]))
+          } else if (json.length === 0) {
+            return _send.call(this, '{}', 404)
+          }
+        }
+      } catch (e) {}
+    }
+    return _send.call(this, body)
+  }
+  next()
+})
+server.use(function (req, res, next) {
+  if (req.method === 'POST') {
+    // Converts POST to GET and move payload to query params
+    // This way it will make JSON Server that it's GET request
+    req.method = 'GET'
+    req.query = req.body
+  }
+  // Continue to JSON Server router
+  next()
+})
 
 server.use(router)
 
