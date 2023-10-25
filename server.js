87df02aa16621e5d776566cf18218d7557318a0c
AlexGooday <alexey.mostovoy.w@gmail.com>
2023-10-25 16:44:21 +0300

    Updated login validation

--- server.js
+++ server.js
@@ -4,19 +4,32 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000
 
-server.use(middlewares)
+
 server.use(jsonServer.rewriter({
    '/api/*': '/$1',
    '/policies/': '/policy',
    '/policies/email/': '/policies',
-   '/auth-service/oauth/token?username=:email': '/users/:email/login?singular=1'
+   '/auth-service/oauth/token\\?*': '/users/sample/login?singular=1'
 }))
 
 server.use(jsonServer.bodyParser)
 
 server.use(function (req, res, next) {
+  if (req.method === 'POST') {
+    // Converts POST to GET and move payload to query params
+    // This way it will make JSON Server that it's GET request
+    req.method = 'GET'
+    req.query = req.body
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+server.use(function (req, res, next) {
   const _send = res.send
   res.send = function (body) {
+
+  console.log(req.method, req.url, req.query)
     if (require('url').parse(req.originalUrl, true).query['singular']) {
       try {
         const json = JSON.parse(body)
@@ -33,16 +46,9 @@ server.use(function (req, res, next) {
   }
   next()
 })
-server.use(function (req, res, next) {
-  if (req.method === 'POST') {
-    // Converts POST to GET and move payload to query params
-    // This way it will make JSON Server that it's GET request
-    req.method = 'GET'
-    req.query = req.body
-  }
-  // Continue to JSON Server router
-  next()
-})
+
+
+server.use(middlewares)
 
 server.use(router)
 
