e9c9cf9aa97f386de5494f5509cd8fe47cc1e774
AlexGooday <alexey.mostovoy.w@gmail.com>
2023-10-30 15:19:09 +0200

    Bugfixes

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const port = process.env.PORT || 3000
 
 server.use(jsonServer.rewriter({
    '/api/*': '/$1',
-   '/policies\\?*': '/policies_list',
    '/policies/email\\?*': '/policies_list',
+   '/policies/email/\\?*': '/policies_list',
+   '/policies\\?*': '/policies_list',
    '/auth-service/oauth/token\\?*': '/users/sample/login?singular=1',
    '/TravelSmart-Allyz-bff/1.0/initialization-data': '/initialization-data',
    '/TravelSmart-Trip-bff/1.0/profile': '/profile',
